be95a8eae89eed1293b309e0d4492eda2ca803b3
sweny <swenyzaveri@yahoo.co.in>
2014-12-08 21:12:00 -0800

    Updated query
    Updated query

--- ResourceAllocMobileCloud/src/com/database/connection/SQLQueries.java
+++ ResourceAllocMobileCloud/src/com/database/connection/SQLQueries.java
@@ -4,7 +4,7 @@ public class SQLQueries {
 
 	public static final String SQL_GET_RESOURCES = "SELECT * FROM customer_resources WHERE instance_id =?";
 	public static final String SQL_GET_SERVERINFO = "SELECT * FROM serverInfo";
-	public static final String SQL_Update_ServerInfo = "Update ServerInfo set RAM = ? and Storage = ? where serverId =?";
+	public static final String SQL_Update_ServerInfo = "Update ServerInfo set RAM = ?, Storage = ? where serverId =?";
 	public static final String SQL_Insert_Utilization = "INSERT into utilizationInfo (UserId, ServerId, RAM, Storage, Location, Device, Network, StartDate) values (?,?,?,?,?,?,?, Sysdate())";
 	public static final String SQL_Update_ServerInfoResource = "Update serverInfo set RAM = 10000";
 }
